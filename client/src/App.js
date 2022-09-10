7576b139dbde8187fb500423db7c1398a93847cf
Afamefuna Anekeh <Afamefuna.Anekeh@episerver.com>
2022-09-10 16:39:11 -0500

    fix nav issue - homepage loading now

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes,} from "react-router-dom";
 import './App.css';
-import { BrowserRouter, Route, Routes,} from "react-router-dom"
-import Nav from './components/Nav/nav'
+import Nav from './components/Nav/nav';
 import Home from './pages/Home/home';
 // import How_it_work from './pages/Home/how_it_work';
 // import Wheel from './pages/Home/wheel';
 // import Donate from './pages/Home/donate';
-// import Contact from './pages/Home/contact';
+import Contact from './pages/Contact/contact';
 
 
 
@@ -15,11 +16,11 @@ function App() {
     <BrowserRouter>
       <Nav />
           <Routes>
-          <Route exact path="/home" component ={Home}></Route>
+          <Route exact path="/home" element ={<Home/>}></Route>
           {/* <Route exact path="/how_it_work" component ={How_it_work}></Route>
           <Route exact path="/wheel" component ={Wheel}></Route>
-          <Route exact path="/donate" component ={Donate}></Route>
-          <Route exact path="/contact" component ={Contact}></Route>           */}
+          <Route exact path="/donate" component ={Donate}></Route> */}
+          <Route exact path="/contact" element ={<Contact/>}></Route>          
           
           </Routes>
        
--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -12,11 +12,8 @@ export default function Main() {
                     <h1 className="mt-10 sm:animate-float text-transparent bg-clip-text bg-gradient-to-t from-blueGray-400 to-blueGray-100 text-6xl font-bold custom-font text-center leading-none lg:leading-snug home-name">
                         Afam Anekeh, Certified Full Stack Developer.
                         
-                    </h1>      
-                    
-                    <img id="profilePic" src="./assets/images/ME.jpg" alt="me" />
-                    <br/>
-                    <br/>             
+                    </h1>                    
+                          
                   
                                       
 
